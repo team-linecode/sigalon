4fc355c36c7d3a49a25174ecf13f8860743d5ba0
team-linecode <linecode.team@gmail.com>
2021-05-15 16:12:32 +0700

    update user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,7 +6,7 @@ class User extends CI_Controller
 	public function index()
 	{
 		$this->form_validation->set_rules('name', 'Name', 'required');
-		$this->form_validation->set_rules('username', 'Username', 'required');
+		$this->form_validation->set_rules('username', 'Username', 'required|is_unique[users.username]');
 		$this->form_validation->set_rules('password', 'Password', 'required');
 		$this->form_validation->set_rules('phone', 'Phone', 'required|is_unique[users.phone]');
 		$this->form_validation->set_rules('level', 'Level', 'required');
@@ -31,7 +31,49 @@ class User extends CI_Controller
 			redirect('user');
 		}
 	}
-	
+
+	public function edit($id)
+	{
+		$this->form_validation->set_rules('name', 'Name', 'required');
+		$this->form_validation->set_rules('username', 'Username', 'required');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+		$this->form_validation->set_rules('phone', 'Phone', 'required');
+		$this->form_validation->set_rules('level', 'Level', 'required');
+
+		if ($this->form_validation->run() == FALSE) {
+			$data['title'] = 'Edit User';
+			$data['user'] = $this->db->get_where('users', ['id' => $id])->row();
+			$this->load->view('layout/admin/header', $data);
+			$this->load->view('admin/user/edit', $data);
+			$this->load->view('layout/admin/footer');
+		} else {
+			$user = $this->db->get_where('users', ['id' => $id])->row();
+			$check_username = $this->db->get_where('users', ['username' => $this->input->post('username')])->result();
+			$check_phone = $this->db->get_where('users', ['phone' => $this->input->post('phone')])->result();
+
+			if (count($check_username) > 0 && $user->username != $this->input->post('username')) {
+				redirect('user/edit/' . $id);
+			}
+
+			if (count($check_phone) > 0 && $user->phone != $this->input->post('phone')) {
+				redirect('user/edit/' . $id);
+			}
+
+			$data = [
+				'name' => $this->input->post('name'),
+				'username' => $this->input->post('username'),
+				'password' => password_hash($this->input->post('password'), PASSWORD_BCRYPT),
+				'nohash' => $this->input->post('password'),
+				'phone' => $this->input->post('phone'),
+				'level' => $this->input->post('level')
+			];
+
+			$this->db->where('id', $id);
+			$this->db->update('users', $data);
+			redirect('user');
+		}
+	}
+
 	public function delete($id)
 	{
 		$this->db->where('id', $id);
---
+++ application/views/admin/user/edit.php
@@ -0,0 +1,43 @@
+<div class="row">
+	<div class="col-lg-6">
+		<div class="card border-0 shadow">
+			<div class="card-body">
+				<form action="<?= base_url('user/edit/' .  $user->id) ?>" method="POST">
+					<div class="form-group">
+						<label>Nama</label>
+						<input type="text" class="form-control" name="name" value="<?= $user->name ?>">
+						<?php echo form_error('name', '<small class="text-danger">', '</small>'); ?>
+					</div>
+					<div class="form-group">
+						<label>Username</label>
+						<input type="text" class="form-control" name="username" value="<?= $user->username ?>">
+						<?php echo form_error('username', '<small class="text-danger">', '</small>'); ?>
+					</div>
+					<div class="form-group">
+						<label>Password</label>
+						<input type="password" class="form-control" name="password" value="<?= $user->nohash ?>">
+						<?php echo form_error('password', '<small class="text-danger">', '</small>'); ?>
+					</div>
+					<div class="form-group">
+						<label>No. Telepon</label>
+						<input type="text" class="form-control" name="phone" value="<?= $user->phone ?>">
+						<?php echo form_error('password', '<small class="text-danger">', '</small>'); ?>
+					</div>
+					<div class="form-group">
+						<label>Level</label>
+						<select name="level" class="custom-select">
+							<option value="">Pilih Level</option>
+							<option value="Admin" <?= $user->level == 'Admin' ? 'selected' : '' ?>>Admin</option>
+							<option value="Customer" <?= $user->level == 'Customer' ? 'selected' : '' ?>>Customer</option>
+						</select>
+						<?php echo form_error('level', '<small class="text-danger">', '</small>'); ?>
+					</div>
+
+					<div class="d-flex justify-content-end">
+						<button class="btn btn-primary"><i class="fas fa-save"></i> Simpan perubahan</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
